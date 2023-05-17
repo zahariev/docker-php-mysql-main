22e1dc2048951e2be568290777727d99f3cab1f3
zahariev <zaharievboyan@gmail.com>
2023-05-17 20:34:32 +0300

    fix: products

--- src/html/export_order_csv.php
+++ src/html/export_order_csv.php
@@ -2,26 +2,68 @@
 
 // $conn = require_once '../php/connection.inc';
 require_once '../php/orderExportData.php';
+$conn = require_once '../php/connection.php';
+$filename = 'orders_' . $start_date . '_' . $end_date . '.csv';
+
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename=' . $filename);
+
+    $file = fopen('php://output', 'w');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $start_date = $_POST['start_date'];
     $end_date = $_POST['end_date'];
 
-    echo "Start Date: $start_date<br>";
-    echo "End Date: $end_date<br>";
-    $data = orderExportData($start_date, $end_date);
-
-    echo $data->num_rows;
-
-    while ($row = $data->fetch_assoc()) {
-    echo "<pre>".print_r($row)."</pre>";
-    }
-
     // Извличане на поръчките и свързаните с тях продукти за избрания период
-    // $data = orderExportData($start_date, $end_date);
+    $data = orderExportData($conn,$start_date, $end_date);
 
+     $header_row = array(
+        'Номер на поръчка',
+        'Малко име',
+        'Фамилия',
+        'Телефон',
+        'И-мейл',
+        'Коментар към поръчка',
+        'Дата на поръчка',
+        'Начин на доставка',
+        'Адрес на доставка',
+        'Статус на поръчка',
+        'Начин на плащане',
+        'Тип на фактурата',
+        'Име на фирма',
+        'МОЛ на фирма',
+        'Седалище на фирма',
+        'ЕИК на фирма',
+        'ддс',
+        'Име за фактура',
+        'Фамилия за фактура',
+        'Адрес за фактура',
+        'ЕГН за фактура'
+    );
+
+
+    if (!empty($data)) {
+        // $order = $data[0];
+
+        fputcsv($file, $header_row);
+
+    
+        foreach ($data as $row) {
+
+               // get products array
+            $products = getProductsArray($row['products']);
+            // array_pop($row);
+
+            fputcsv($file, $row);
+             foreach ($products as $product) 
+                            fputcsv($file, $product);
+        }
+    } else {
+        fputcsv($file, array('No orders found'));    
+    }
 }
 
+ fclose($file);
     exit;
 
     ?>
--- src/php/orderExportData.php
+++ src/php/orderExportData.php
@@ -1,67 +1,66 @@
 <?
 
 
-function orderExportData($startDate, $endDate) 
+function orderExportData($conn, $startDate, $endDate) 
 {
-
-    $conn = require_once 'connection.php';
-
     // Prepare the SQL query
     $sql = "SELECT
-    o.id AS 'Номер на поръчка',
-    o.name AS 'Малко име',
-    o.family AS 'Фамилия',
-    o.phone AS 'Телефон',
-    o.email AS 'И-мейл',
-    o.comment AS 'Коментар към поръчка',
-    o.updated_at AS 'Дата на поръчка',
-    o.delivery_type AS 'Начин на доставка',
+    o.id ,
+    o.name ,
+    o.family,
+    o.phone ,
+    o.email,
+    o.comment,
+    o.updated_at,
+    o.delivery_type,
     CASE
         WHEN o.delivery_type LIKE 'До адрес%' THEN CONCAT(o.city, ' ', o.address)
         WHEN o.delivery_type LIKE 'Офис на%' THEN CONCAT('Oфис no. ', o.office)
         ELSE ''
-    END AS 'Адрес на доставка',
-    o.status AS 'Статус на поръчка',
-    o.payment_type AS 'Начин на плащане',
-    o.invoice_type AS 'Тип на фактурата',
+    END AS delivery_address,
+    os.name AS status,
+    pt.name AS payment_type,
+    o.invoice_type ,
     CASE
         WHEN o.invoice_type = 'Юридическо лице' THEN o.invoice_j_firm
         ELSE ''
-    END AS 'Име на фирма',
+    END AS 'firm_name',
     CASE
         WHEN o.invoice_type = 'Юридическо лице' THEN o.invoice_j_mol
         ELSE ''
-    END AS 'МОЛ на фирма',
+    END AS 'mol',
     CASE
         WHEN o.invoice_type = 'Юридическо лице' THEN o.invoice_j_address
         ELSE ''
-    END AS 'Седалище на фирма',
+    END AS 'firm_address',
     CASE
         WHEN o.invoice_type = 'Юридическо лице' THEN o.invoice_j_eik
         ELSE ''
-    END AS 'ЕИК на фирма',
+    END AS 'eik',
     CASE
-        WHEN o.invoice_type = 'Юридическо лице' THEN o.invoice_j_has_vat
+        WHEN o.invoice_type = 'Юридическо лице' AND o.invoice_j_has_vat THEN 'Да' 
         ELSE ''
-    END AS ' ДДС',
+    END AS ' vat',
     CASE
         WHEN o.invoice_type = 'Физическо лице' THEN o.invoice_p_name
         ELSE ''
-    END AS 'Име за фактура',
+    END AS 'invoice_name',
     CASE
         WHEN o.invoice_type = 'Физическо лице' THEN o.invoice_p_family
         ELSE ''
-    END AS 'Фамилия за фактура',
+    END AS 'invoice_family',
     CASE
         WHEN o.invoice_type = 'Физическо лице' THEN o.invoice_p_address
         ELSE ''
-    END AS 'Адрес за фактура',
+    END AS 'invoice_address',
     CASE
         WHEN o.invoice_type = 'Физическо лице' THEN o.invoice_p_egn
         ELSE ''
-    END AS 'ЕГН за фактура'
+    END AS 'invoice_egn'
    
 FROM orders o
+LEFT JOIN order_statuses os ON o.status = os.id
+LEFT JOIN payment_types pt ON o.payment_type = pt.id
 WHERE DATE_FORMAT(o.updated_at,'%Y-%m-%d') BETWEEN DATE_FORMAT( ? ,'%Y-%m-%d') AND DATE_FORMAT(?,'%Y-%m-%d') 
 ORDER BY o.id ASC";
 
@@ -71,16 +70,70 @@ ORDER BY o.id ASC";
     if ($stmt === false) {
     die("Error in preparing the SQL query: " . $conn->error);
 }
+  
 
     // Bind the parameters
-    $stmt->bind_param('ss', $startDate, $endDate);
-
+  $stmt->bind_param('ss', $startDate, $endDate);
     // Execute the query
     $stmt->execute();
 
     // Get the result set
     $result = $stmt->get_result();
-    return $result;//->fetch_all(MYSQLI_ASSOC);
+
+     while ($row = $result->fetch_assoc()) {
+
+        $order = $row;
+        $order['products'] = getOrderProducts($conn, $row['id']);
+        $orders[] =  $order;
+    }
+
+    return $orders;
+}
+
+function getOrderProducts($conn, $orderId) {
+    $sql = "SELECT full_arr FROM order_products WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $orderId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $products = array();
+
+    while ($row = $result->fetch_assoc()) {
+    //     $obj = json_decode($row['full_arr']);
+    // //    echo "<pre>".($row['full_arr'])."</pre>";
+    //   print_r($obj);
+
+        $products[] = json_decode($row['full_arr']);
+    }
+    
+    return $products;
+}
+
+function getProductsArray($productsString) {
+  
+    $resultArray = array();
+
+    $resultArray[] = array(
+                                'ID',
+                                'Име на продукта',
+                                'Бр.',
+                                'Цена',
+                                'Сума',
+                );
+            
+            foreach ($productsString as $product) {
+                $resultArray[] = array(
+                   $product->product_id,
+                    $product->title,
+                    $product->quantity,
+                    $product->price,
+                    $product->real_price,
+                );
+
+            }
+   
+
+    return $resultArray;
 }
 
 ?>
